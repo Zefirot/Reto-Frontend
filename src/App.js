cba4f8e534d9a476fe3b8bc0287a0f61865415cc
Campos Gastón <gaston.campos509@gmail.com>
2022-05-21 04:26:58 -0300

    [Update] Se implemento la grabacion-stop y play

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import MediaRecorder from "./components//MediaRecorder/MediaRecorder";
+import RecordWindow from "./components//MediaRecorder/MediaRecorder";
 import "./components/MediaRecorder/MediaRecorder.css";
 
 function App() {
   return (
     <div>
-      <MediaRecorder />
+      <RecordWindow />
     </div>
   );
 }
--- src/components/MediaRecorder/MediaRecorder.jsx
+++ src/components/MediaRecorder/MediaRecorder.jsx
@@ -1,19 +1,12 @@
-import React, {useRef} from 'react';
-//import './src/components/MediaRecorder/MediaRecorder.css';
+import React, { useRef } from 'react';
 
-export default function MediaRecorder() {
+export default function RecordWindow() {
   let mediaRecorder;
   let recordedBlobs;
   const myContainer = useRef();
-  /* const style = {
-    '--width':'25vw',
-    'width': 'var(--width)',
-    'height':'calc(var(--width) * 0.5625)',
-    'background': '#222'
-  }
- */
+
   const webCamOn = async () => {
-    const constraints = {audio: true, video: {width: 1280, height: 720}};
+    const constraints = { audio: true, video: { width: 1280, height: 720 } };
 
     try {
       const stream = await navigator.mediaDevices.getUserMedia(constraints);
@@ -21,58 +14,59 @@ export default function MediaRecorder() {
 
       myContainer.current.srcObject = stream;
       myContainer.current.play()
+
     } catch (e) {
       console.error('navigator.getUserMedia error:', e);
     }
   };
 
+  const startRecording = async () => {
+    await webCamOn()
+
+    recordedBlobs = [];
+
+    try {
+      mediaRecorder = new MediaRecorder(window.stream);
+    } catch (e) {
+      console.error('Exception while creating MediaRecorder:', e);
+      return;
+    }
+
+    console.log('Created MediaRecorder', mediaRecorder);
+    mediaRecorder.onstop = (event) => {
+      console.log('Recorder stopped: ', event);
+      console.log('Recorded Blobs: ', recordedBlobs);
+    };
+    mediaRecorder.ondataavailable = handleDataAvailable;
+    mediaRecorder.start();
+    console.log('MediaRecorder started', mediaRecorder);
+  }
+
+  const stopRecording = () => {
+    mediaRecorder.stop();
+  }
+
+  const handleDataAvailable = (event) => {
+    if (event.data && event.data.size > 0) {
+      recordedBlobs.push(event.data);
+    }
+  }
+
+  const playVideo = () => {
+    const superBuffer = new Blob(recordedBlobs);
+    myContainer.current.src = null;
+    myContainer.current.srcObject = null;
+    myContainer.current.src = window.URL.createObjectURL(superBuffer);
+    myContainer.current.controls = true;
+    myContainer.current.play();
+  }
+
   return (
     <div>
-      <video ref={myContainer} >
-        <button>asd</button>
-      </video>
-      <button onClick={webCamOn}>Start</button>
-      <button>Stop</button>
+      <video ref={myContainer}></video>
+      <button onClick={startRecording}>Start</button>
+      <button onClick={stopRecording}>Stop</button>
+      <button onClick={playVideo}>Play Record</button>
     </div>
   )
 }
-
-
-
-
-/* function startRecording() {
-  recordedBlobs = [];
-
-  try {
-    mediaRecorder = new MediaRecorder(window.stream);
-  } catch (e) {
-    console.error('Exception while creating MediaRecorder:', e);
-    errorMsgElement.innerHTML = `Exception while creating MediaRecorder: ${JSON.stringify(e)}`;
-    return;
-  }
-
-  console.log('Created MediaRecorder', mediaRecorder, 'with options', options);
-  recordButton.textContent = 'Stop Recording';
-  mediaRecorder.onstop = (event) => {
-    console.log('Recorder stopped: ', event);
-    console.log('Recorded Blobs: ', recordedBlobs);
-  };
-  mediaRecorder.ondataavailable = handleDataAvailable;
-  mediaRecorder.start();
-  console.log('MediaRecorder started', mediaRecorder);
-}
-function stopRecording() {
-  mediaRecorder.stop();
-} */
-
-
-/* const playButton = document.querySelector('button#play');
-playButton.addEventListener('click', () => {
-  const mimeType = codecPreferences.options[codecPreferences.selectedIndex].value.split(';', 1)[0];
-  const superBuffer = new Blob(recordedBlobs, {type: mimeType});
-  recordedVideo.src = null;
-  recordedVideo.srcObject = null;
-  recordedVideo.src = window.URL.createObjectURL(superBuffer);
-  recordedVideo.controls = true;
-  recordedVideo.play();
-}); */
